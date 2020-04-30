1d51c849f05c7b9b03718bf12023c666f945a4b1
krtggayathri <54623500+krtggayathri@users.noreply.github.com>
2020-04-30 21:58:21 +0530

    Update assignment_003.py

--- assignment_003.py
+++ assignment_003.py
@@ -69,34 +69,15 @@ Q6="Write your query here"
 
 
 __________________________________________________________________________________________________________________
+Q1='SELECT COUNT(name) FROM Movie WHERE year=1991'
 
-Q1='SELECT * FROM Movie  ORDER BY rank DESC LIMIT 10;'
+Q2='SELECT MIN(rank) FROM Movie'
 
-Q2='SELECT * FROM Movie  ORDER BY rank DESC LIMIT 10 OFFSET 10;'
+Q3='SELECT MAX(rank) FROM Movie WHERE year=2000'
 
-Q3='SELECT name FROM Movie WHERE year>2004'
+Q4='SELECT AVG(rank) FROM Movie WHERE year=2000'
 
-Q4='SELECT name FROM Movie WHERE rank<1.1'
+Q5='SELECT COUNT(DISTINCT year) FROM Movie'
 
-Q5='SELECT * FROM Movie WHERE year IN (2004,2005,2006)'
-
-Q6='SELECT name,year FROM Movie WHERE name LIKE "Harry%";'
-
-Q7='SELECT * FROM Actor WHERE (fname="Christin") AND (lname!="Watson")'
-
-Q8='SELECT * FROM Actor WHERE (fname="Woody") AND (lname="Watson")'
-
-Q9='SELECT name FROM Movie WHERE year=1990 AND rank=5'
-
-Q10='SELECT * FROM Actor WHERE (fname="Christin") AND (lname="Watson")'
-
-Q11='SELECT name FROM Movie WHERE year BETWEEN 2003 AND 2005'
-
-Q12='SELECT DISTINCT year FROM Movie ORDER BY year ASC'
-
-Q13='''SELECT * FROM Actor WHERE (fname="Christin") OR (lname="Watson") AND 
-        (gender="M") ORDER BY fname ASC LIMIT 10'''
-____________________________________________________________________________________
-
-
+Q6='SELECT MIN(year),MAX(year) FROM Movie'
 
