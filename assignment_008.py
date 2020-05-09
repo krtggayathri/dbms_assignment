1b59a8a2fc3a51146d05e1b15a5327dd1d92a165
krtggayathri <54623500+krtggayathri@users.noreply.github.com>
2020-05-09 21:49:44 +0530

    Update assignment_008.py

--- assignment_008.py
+++ assignment_008.py
@@ -33,4 +33,29 @@ Q2="Write your query here"
 
 List 100 actors who didn't act in any movie between 1990 and 2000. Your query should return only 100 unique actors when sorted by id in descending order.
 
-Q3="Write your query here"    
+Q3="Write your query here"  
+
+
+_______________________________________________________________________________
+
+Q1='''SELECT d.id,d.fname FROM Director as d  
+WHERE NOT EXISTS (SELECT m.did FROM MovieDirector as m INNER JOIN Movie as l on l.id=m.mid  
+                                WHERE d.id=m.did and year<2000)
+AND EXISTS (SELECT m.did FROM  MovieDirector as m INNER JOIN Movie as l on l.id=m.mid  
+        WHERE d.id=m.did and year>2000) ORDER BY d.id ASC;'''
+        
+
+Q3='''SELECT * FROM Actor as ac
+WHERE NOT EXISTS (SELECT c.pid FROM Cast as c INNER JOIN Movie as l on l.id=c.mid
+WHERE ac.id=c.pid and year BETWEEN 1990 AND 2000) order by ac.id DESC LIMIT 100;'''
+
+
+
+
+
+Q2='''SELECT d.fname,(SELECT l.name FROM Movie as l INNER JOIN MovieDirector as md on l.id=md.mid WHERE md.did=d.id order by rank desc,name asc) 
+ FROM director as d limit 100;'''
+ 
+
+
+
